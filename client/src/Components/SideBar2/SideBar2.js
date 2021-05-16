114f182e4409a12f6e802ddc0c2576c5b5c37fcb
Jared de Uriarte <jared.deuri@gmail.com>
2021-05-16 15:20:19 -0400

    sidebar playing

--- client/src/Components/SideBar2/SideBar2.js
+++ client/src/Components/SideBar2/SideBar2.js
@@ -7,7 +7,7 @@ const Sidebar2 = (props) => {
     <div id="sidebar" className="col-2 m-2">
       <h5 className="h4 text-center font-weight-bold mt-3">{props.title}</h5>
       <SideBarCard title="CSS" />
-      <SideBarCard title="HTML%" />
+      <SideBarCard title="HTML" />
       <SideBarCard title="NodeJS" />
       <SideBarCard title="JavaScript" />
       <SideBarCard title="MySQL" />
