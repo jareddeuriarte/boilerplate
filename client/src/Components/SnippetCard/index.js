9affed8663971b2ed2a6013e4c86fbfb1611ddee
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-05-16 15:15:34 -0400

    added a bit of margin around the cards

--- client/src/Components/SnippetCard/index.js
+++ client/src/Components/SnippetCard/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function SnippetCard({ title, code, tag }) {
   return (
-    <div className="card-wrapper">
+    <div className="card-wrapper m-3">
       <div className="snippet-title">{title}</div>
       <pre className="code-block">
         <code>{code}</code>
