95a5f565daf2d16927a262b8eaf64da7e6dff018
Jared de Uriarte <jared.deuri@gmail.com>
2021-05-16 19:06:17 -0400

    when user clicks on code block, the whole block is hselected

--- client/src/Components/SnippetCard/index.js
+++ client/src/Components/SnippetCard/index.js
@@ -5,9 +5,12 @@ function SnippetCard({ title, code, tag }) {
   return (
     <div className="card-wrapper m-3">
       <div className="snippet-title">{title}</div>
-      <pre className="code-block">
-        <code>{code}</code>
-      </pre>
+
+    
+        <pre className="code-block">
+          <code>{code}</code>
+        </pre>
+
       <div className="bottom-row">
         <div className="snippet-tags">tags: {tag}</div>
         <span role="img" aria-label="heart-emoji">
