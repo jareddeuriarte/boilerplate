f7bcd65e1dc49c66022f0af97ac0373994313213
Jared de Uriarte <jared.deuri@gmail.com>
2021-05-16 21:12:37 -0400

    clipboard emoji

--- client/src/Components/SnippetCard/index.js
+++ client/src/Components/SnippetCard/index.js
@@ -2,22 +2,23 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 
 function SnippetCard({ title, code, tag }) {
+
   return (
     <div className="card-wrapper m-3">
       <div className="snippet-title">{title}</div>
 
-    
-        <pre className="code-block">
-          <code>{code}</code>
-        </pre>
+
+      <pre className="code-block">
+        <code id='copy-code'>{code}</code>
+      </pre>
 
       <div className="bottom-row">
         <div className="snippet-tags">tags: {tag}</div>
-        <span role="img" aria-label="heart-emoji">
-          💟
-        </span>
+        <span role='img' aria-label='copy-to-clipboard'>📋</span>
       </div>
+
     </div>
+
   );
 }
 
